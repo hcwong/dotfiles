aca6bcc87c1f39b0a3c61d714b0b51afb56d559d
Joshua Wong [Intern] <wongjhc@C02RX13BG8WM.local>
2020-05-11 23:23:22 +0800

    Fix typos

--- setup.sh
+++ setup.sh
@@ -1,29 +1,45 @@
 # To do prior to setup
 # 1. Install Git + Homebrew
 # 2. Install Zsh
-# 3. Install neovim
-# 4. Comment out last line of zshrc
+# 3. Comment out last line of zshrc
 # No linux, you need a Mac for this cos homebrew dependencies, guess that's another #TODO
 
+zsh
+
+cd ~/
+
 # Set links, assuming that cwd is in the dotfiles folder
+# TODO: not copy git over with the globbing
 setopt EXTENDED_GLOB
-for file in ./^setup.sh(.N); do
+for file in ./dotfiles/{,.}*; do
   ln -s "$file" "$HOME/${file:t}"
 done
 
-# Install all homebrew packages
-brew install "$(grep . homebrew.txt | tr '\n' ' ')"
-brew cask install "$(grep . homebrewcask.txt | tr '\n' ' ')"
-
 # Tmux
-ln -s ~/tmux/tmux.conf ~/.tmux.conf
+ln -s ./tmux/tmux.conf ~/.tmux.conf
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Neovim setup
-mkdir -p .config
-mkdir -p .config/nvim
-ln -s ~/nvim/init.vim .config/nvim/init.vim
-ln -s ~/nvim/coc-settings.json .config/nvim/coc-settings.json
+mkdir "$HOME/.config"
+mkdir "$HOME/.config/nvim"
+cd ~/.config/nvim
+ln -s ../../dotfiles/nvim/init.vim init.vim
+ln -s ../../dotfiles/nvim/coc-settings.json coc-settings.json
+
+cd ~/dotfiles
+
+# Install all homebrew packages
+brew install $(grep . homebrew.txt | tr '\n' ' ')
+brew cask install $(grep . homebrewcask.txt | tr '\n' ' ')
+
+# Copy over zprezto setup
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 # Launch and change default shell to zsh
-zsh
+# If issues, use this link: https://rick.cogley.info/post/use-homebrew-zsh-instead-of-the-osx-default/
 chsh -s /bin/zsh
